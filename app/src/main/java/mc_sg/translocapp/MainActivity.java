f2e360103c5b62f18e62193ea579d19210889670
Steven Gatto <steven.gatto001@umb.edu>
2015-10-26 23:42:13 -0400

    Unstable draft of custom map info windows

--- app/src/main/java/mc_sg/translocapp/MainActivity.java
+++ app/src/main/java/mc_sg/translocapp/MainActivity.java
@@ -5,9 +5,13 @@ import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -19,6 +23,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.List;
@@ -26,12 +31,19 @@ import java.util.List;
 import mc_sg.translocapp.model.Agency;
 import mc_sg.translocapp.model.Response;
 import mc_sg.translocapp.network.ApiUtil;
+import mc_sg.translocapp.view.MapWrapperLayout;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private Activity context;
     private GoogleApiClient mGoogleApiClient;
     private GoogleMap map;
+    private MapWrapperLayout mapWrapper;
+    private ViewGroup infoWindow;
+    private TextView infoTitle;
+    private TextView infoSnippet;
+    private Button infoButton;
+    private View.OnTouchListener infoWindowListener;
     private boolean mapInitialized = false;
 
     @Override
@@ -40,6 +52,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         context = this;
         buildGoogleApiClient();
+
+        // We want to reuse the info window for all the markers,
+        // so let's create only one class member instance
+        mapWrapper = (MapWrapperLayout) findViewById(R.id.map_wrapper);
+        infoWindow = (ViewGroup)getLayoutInflater().inflate(R.layout.view_map_info_window, null);
+        infoTitle = (TextView) infoWindow.findViewById(R.id.title);
+        infoSnippet = (TextView) infoWindow.findViewById(R.id.snippet);
+        infoButton = (Button) infoWindow.findViewById(R.id.button);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(null, "marker button clicked");
+            }
+        });
 
         Button btnGet = (Button) findViewById(R.id.btn_get);
         btnGet.setOnClickListener(new View.OnClickListener() {
@@ -81,6 +107,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.setMyLocationEnabled(true);
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         initMapPosition();
+
+        mapWrapper.init(map, getPixelsFromDp(this, 39 + 20));
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Setting up the infoWindow with current's marker info
+                infoTitle.setText(marker.getTitle());
+                infoSnippet.setText(marker.getSnippet());
+
+                // We must call this to set the current marker and infoWindow references
+                // to the MapWrapperLayout
+                mapWrapper.setMarkerWithInfoWindow(marker, infoWindow);
+                return infoWindow;
+            }
+        });
     }
 
     private void initMapPosition() {
@@ -90,6 +136,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
             mapInitialized = true;
         }
+    }
+
+    public static int getPixelsFromDp(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dp * scale + 0.5f);
     }
 
     private class GoogleApiConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
